b3d62c2c3f11955b0fdbe86c45db03929553d9c1
Tim Blanchard <timothy.blanchard@tesco.com>
2020-08-18 19:22:07 +0100

    value to return tests passing

--- index.test.js
+++ index.test.js
@@ -1,9 +1,19 @@
-import {foo} from '.'
+import {foo} from './valueToReturn'
+describe('valueToReturn', () => {
+  it('should return 0 if price === amount given', () => {
+    expect(valueToReturn(5, 5)).toEqual(0)
+  });
 
-test('should ', () => {
- expect(foo()).toEqual('foo')
-});
+  it('should return 0 if price > amount given', () => {
+    expect(valueToReturn(6, 5)).toEqual(0)
+  });
 
-test('should ', () => {
-  expect(foo).toEqual('bar')
+  it('should calculate value correctly', () => {
+    expect(valueToReturn(5, 6)).toEqual(1)
+  });
+
+  it('should handle decimals correctly', () => {
+    expect(valueToReturn(5.60, 6.25)).toEqual(0.65)
+  });
+
 });
